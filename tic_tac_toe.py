d17382c1a13c532803d621fb442285434e91c754
EvgenyPobozhiy <pobozhyevg@mail.ru>
2023-04-23 16:14:31 +0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,100 +1,78 @@
-PLAYER_SYMBOLS = "-OX"
-ROW_INDICES = "ABC"
-COLUMN_INDICES = "123"
+def greet():
+	print("---------------")
+	print("КРЕСТИКИ_НОЛИКИ")
+	print("---------------")
+	print("Формат ввода: х у \nх - номер строки \nу - номер столбца ")
+	print("---------------")
 
-def print_game_map(gm):
-    for row in gm:
-        for cell in row:
-            print(PLAYER_SYMBOLS[cell], end=" ")
-        print()
+def show():
+	print()
+	print("   | 0 | 1 | 2 | ")
+	print(" --------------- ")
+	for i,row in enumerate(field):
+		row_str = f" {i} | {' | '.join(row)} | "
+		print(row_str)
+		print(" --------------- ")
+	print()
 
-def is_tie(gm):
-    for row in gm:
-        for cell in row:
-            if cell == 0:
-                return False
-    return True
+def ask():
+	while True:
+		cords = input("Ваш ход: ").split()
 
-def is_completed(gm):
-    for row in gm:
-        first = row[0]
-        if first != 0:
-            for cell in row[1:]:
-                if cell != first:
-                    break
-            else:
-                return True, first
-    for j in range(len(gm[0])):
-        first = gm[0][j]
-        if first != 0:
-            for i in range(1, len(gm)):
-                if gm[i][j] != first:
-                    break
-                else:
-                    return True, first
-    diag = gm[0][0]
-    if diag != 0:
-        for i in range(len(gm)):
-            if gm[i][i] != diag:
-                break
-        else:
-            return True, diag
-    inv_diag = gm[0][len(gm[0]) - 1]
-    if inv_diag != 0:
-        for i in range(len(gm)):
-            if gm[i][len(gm) - i - 1] != inv_diag:
-                break
-        else:
-            return True, inv_diag
-    return is_tie(gm), 0
+		if len(cords) != 2:
+			print("Введите 2 координаты! ")
+			continue
+		x, y = cords
+		if not(x.isdigit()) or not(y.isdigit()):
+			print("Введите числа! ")
+			continue
 
-game_map = []
-for _ in range(3):
-    temp = []
-    for _ in range(3):
-        temp.append(0)
-    game_map.append(temp)
+		x, y = int(x), int(y)
+		if 0 > x or x > 2 or 0 > y or y > 2:
+			print("Координаты вне диапазона! ")
+			continue
 
+		if field[x][y] != " ":
+			print("Клетка занята! ")
+			continue
+		return x, y
 
+def check_win():
+	win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+				((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+				((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+	for cord in win_cord:
+		symbols = []
+		for c in cord:
+			symbols.append(field[c[0]][c[1]])
+		if symbols == ["X", "X", "X"]:
+			print("Выиграл X!!!")
+			return True
+		if symbols == ["0", "0", "0"]:
+			print("Выиграл 0!!!")
+			return True
+	return False
 
-current_player = 1
-completed = False
-who_won = 0
-while not completed:
-    print_game_map(game_map)
-    player_input = input(f"Игрок №{current_player}, введите позицию в формате А1: ")
-    if len(player_input) != 2:
-        print("Введено неверное количество символов. Формат ввода: А1")
-        continue
-    player_row, player_column = player_input
+greet()
+field = [[" "] * 3 for i in range(3)]
+num = 0
+while True:
+	num += 1
+	show()
+	if num % 2 == 1:
+		print("Ходит крестик: ")
+	else:
+		print("Ходит нолик: ")
 
-    player_row = ROW_INDICES.find(player_row)
-    if player_row == -1:
-        print("Неверный номер ряда. Укажите букву А, В, С")
-        continue
+	x, y = ask()
+	if num % 2 == 1:
+		field[x][y] = "X"
+	else:
+		field[x][y] = "O"
 
-    player_column = COLUMN_INDICES.find(player_column)
-    if player_column == -1:
-        print("Неверный номер ряда. Укажите цифру 1, 2, 3")
-        continue
+	if check_win():
+		break
 
-    if game_map[player_row][player_column] > 0:
-        print("Ячейка уже занята, выберите другую ")
-        continue
-    game_map[player_row][player_column] = current_player
-
-    completed, who_won = is_completed(game_map)
-
-    if current_player == 1:
-        current_player = 2
-    elif current_player == 2:
-        current_player = 1
-    else:
-        print("Неверный номер игрока")
-        exit(1)
-
-print_game_map(game_map)
-if who_won != 0:
-    print(f"Игрок №{who_won} победил! ")
-else:
-    print("Ничья! ")
+	if num == 9:
+		print("Ничья! ")
+		break
